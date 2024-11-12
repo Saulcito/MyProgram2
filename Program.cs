cb5c449b48771156da8db84971a725762b2f284d
saulp <saulp@LAPTOP-ESAIP1DN>
2024-11-12 18:47:00 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "my name is Saul Pazmino";
+            int age = 19;
+            Console.WriteLine("Hello,{0}, i'm {1}", name, age);
+            Console.ReadKey();
+        }
+    }
+}
